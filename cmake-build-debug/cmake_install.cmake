8c861c3271940b686e54c2ef56d82bf11f200f4c
acse-xg1123 <xueling.gao23@imperial.ac.uk>
2024-03-21 20:14:23 +0000

    update gitignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CW3")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/advanced-programming-group-selection-sort")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
